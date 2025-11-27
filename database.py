395d936fec8c097079a77ab74f8a11c4d1bcaccb
Alice Schmidt <alicesch@ymail.com>
2025-11-27 20:59:27 -0300

    refactor(db): change to neon db connection string

--- database.py
+++ database.py
@@ -8,14 +8,7 @@ import os
 load_dotenv()
 
 # Fetch variables
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-
-# Construct the SQLAlchemy connection string
-DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Create the SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
@@ -24,9 +17,9 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# # Test the connection
-# try:
-#     with engine.connect() as connection:
-#         print("Connection successful!")
-# except Exception as e:
-#     print(f"Failed to connect: {e}")
+# Test the connection
+try:
+    with engine.connect() as connection:
+        print("Connection successful!")
+except Exception as e:
+    print(f"Failed to connect: {e}")
