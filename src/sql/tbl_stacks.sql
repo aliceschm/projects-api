d58bfa6a717287e80204643d0be7bb3a839e24ef
Alice Schmidt <alicesch@ymail.com>
2025-11-30 18:46:06 -0300

    feat(db): add name_normalized to avoid duplicates

--- src/sql/tbl_stacks.sql
+++ src/sql/tbl_stacks.sql
@@ -1,6 +1,9 @@
 /* Table with list of stacks */
 
 CREATE TABLE portfolio.stacks (
-  id    INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  name  TEXT NOT NULL
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    name_normalized TEXT NOT NULL,
+
+    CONSTRAINT stacks_name_normalized_unique UNIQUE (name_normalized)
 );
