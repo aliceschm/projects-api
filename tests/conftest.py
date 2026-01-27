042e84bb063722e123a481e91cb8660416d9fe7b
Alice Schmidt <alicesch@ymail.com>
2026-01-27 18:04:07 +0000

    feat: health test

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,14 @@ import os
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from src.database import Base 
+from src.database import Base
 from src.main import app
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 @pytest.fixture(scope="function")
 def db_session():
